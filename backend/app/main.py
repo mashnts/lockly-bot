52b01954962fa1e02f2c2226b03dea7b5b2a1acb
mashnts <mashentsovaalina@gmail.com>
2026-03-01 13:19:08 +0200

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,15 +4,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.tasks import check_expired_subscriptions
 from aiogram.types import BotCommand
 
-async def set_commands(bot):
-    await bot.set_my_commands([
-        BotCommand(command="start", description="Запустить бота"),
-        BotCommand(command="addchannel", description="Добавить канал"),
-        BotCommand(command="dashboard", description="Мой кабинет"),
-        BotCommand(command="subscribe", description="Подписаться на канал"),
-        BotCommand(command="mysubscriptions", description="Мои подписки"),
-    ])
-
 app = FastAPI(title="Lockly")
 
 app.include_router(authors.router)
--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,25 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from bot.config import settings
 from bot.handlers import start, channel, dashboard, subscribe, my_subscriptions
 
 async def main():
-    bot = Bot(token = settings.BOT_TOKEN)
+    bot = Bot(token=settings.BOT_TOKEN)
     dp = Dispatcher()
-
     dp.include_router(start.router)
     dp.include_router(channel.router)
     dp.include_router(dashboard.router)
     dp.include_router(subscribe.router)
     dp.include_router(my_subscriptions.router)
+
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Запустить бота"),
+        BotCommand(command="addchannel", description="Добавить канал"),
+        BotCommand(command="dashboard", description="Мой кабинет"),
+        BotCommand(command="subscribe", description="Подписаться на канал"),
+        BotCommand(command="mysubscriptions", description="Мои подписки"),
+    ])
 
     await dp.start_polling(bot)
 
